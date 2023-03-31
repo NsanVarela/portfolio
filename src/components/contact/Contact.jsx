e2d41e0f84b013614712422b2f1056308ba0d203
NsanV <nicolas.sanches-varela@pole-emploi.fr>
2023-03-31 11:38:17 +0200

    contact done using emailjs and whatsapp api

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from "react-icons/md";
+import {RiMessengerLine} from "react-icons/ri";
+import {BsWhatsapp} from "react-icons/bs";
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            form.current,
+            process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        ).then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset()
+    };
+
     return (
-        <section id='contact'>Contact</section>
+        <section id='contact'>
+            <h5>Get in Touch</h5>
+            <h2>Contact me</h2>
+
+            <div className='container contact__container'>
+                <div className='contact__options'>
+                    <article className='contact__option'>
+                        <MdOutlineEmail className='contact__option-icon' />
+                        <h4>Email</h4>
+                        <h5>nsanv.dev@gmail.com</h5>
+                        <a href='mailto:nsanv.dev@gmail.com' target='_blank'>Send a message</a>
+                    </article>
+                    {/*<article className='contact__option'>
+                        <RiMessengerLine className='contact__option-icon' />
+                        <h4>Messenger</h4>
+                        <h5>NsanV</h5>
+                        <a href='#' target='_blank'>Send a message</a>
+                    </article>*/}
+                    <article className='contact__option'>
+                        <BsWhatsapp className='contact__option-icon' />
+                        <h4>WhatsApp</h4>
+                        <h5>Nicolas</h5>
+                        <a href='https://wa.me/+33659333550' target='_blank'>Send a message</a>
+                    </article>
+                </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type='text' name='name' placeholder='Your full Name' required />
+                    <input type='email' name='email' placeholder='Your Email' required />
+                    <textarea name='message' rows='7' placeholder='Your message' required ></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
